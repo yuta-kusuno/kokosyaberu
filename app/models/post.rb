a62e1316bf5c3fb0d7f151c2fb3c2fad5ac69559
yuta.kusuno <yuta.ksp@gmail.com>
2020-01-14 04:46:03 +0000

    nitifications→notifications

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
-  has_many :nitifications, dependent: :destroy
+  has_many :notifications, dependent: :destroy
   
   validates :content, presence: true, length:{maximum: 255}
   validates :datetime, :place, length:{maximum: 30}
